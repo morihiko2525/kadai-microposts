84332eb1beed5b786fdde2c4a96e65852a7f3368
Yuki Mori <morihiko223@gmail.com>
2021-04-23 09:19:08 +0000

    first commit

---
+++ app/Micropost.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Micropost extends Model
+{
+    //
+    protected $fillable = ['content'];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function favorite_users(){
+        $this->belongsToMany(Micropost::class, 'favorites', 'micropost_id', 'user_id')->withTimestamps();
+    }
+}
---
+++ resources/views/microposts/microposts.blade.php
@@ -0,0 +1,46 @@
+@if (count($microposts) > 0)
+    <ul class="list-unstyled">
+        @foreach ($microposts as $micropost)
+            <li class="media mb-3">
+                {{-- 投稿の所有者のメールアドレスをもとにGravatarを取得して表示 --}}
+                <img class="mr-2 rounded" src="{{ Gravatar::get($micropost->user->email, ['size' => 50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
+                        {!! link_to_route('users.show', $micropost->user->name, ['user' => $micropost->user->id]) !!}
+                        <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                    </div>
+                    <div>
+                        {{-- 投稿内容 --}}
+                        <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                    </div>
+                    <ul class="list-inline">
+                        @if (!Auth::user()->is_favoriting($micropost->id))
+                        <li class="list-inline-item">
+                            {!! Form::open(['route' => ['favorites.favorite', $micropost->id], 'method' => 'post']) !!}
+                                {!! Form::submit('Favorite', ['class' => 'btn btn-success btn-sm']) !!}
+                            {!! Form::close() !!}
+                        </li>
+                        @else
+                        <li class="list-inline-item">
+                            {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+                                {!! Form::submit('Unfavorite', ['class' => 'btn btn-success btn-sm']) !!}
+                            {!! Form::close() !!}
+                        </li>
+                        @endif
+                        <li class="list-inline-item">
+                            @if (Auth::id() == $micropost->user_id)
+                                {{-- 投稿削除ボタンのフォーム --}}
+                                {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                                    {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                                {!! Form::close() !!}
+                            @endif
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    {{-- ページネーションのリンク --}}
+    {{ $microposts->links() }}
+@endif
